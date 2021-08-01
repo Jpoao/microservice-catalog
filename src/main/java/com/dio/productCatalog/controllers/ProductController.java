f7c370917a1879e353ce9d9cb39b57556e930bfa
João Paulo <mg_ses@hotmail.com>
2021-07-31 22:18:02 -0300

    Fix: controller, add: Find product By Id end point and service  exception

--- src/main/java/com/dio/productCatalog/controllers/ProductController.java
+++ src/main/java/com/dio/productCatalog/controllers/ProductController.java
@@ -4,17 +4,19 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dio.productCatalog.entities.Product;
 import com.dio.productCatalog.services.ProductService;
 
 
-@Controller
+@RestController
 @RequestMapping("/product")
 public class ProductController {
 
@@ -27,5 +29,11 @@ public class ProductController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand()
 				.toUri();
 		return ResponseEntity.created(uri).body(newProduct);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Integer id){
+		Product entity = service.findById(id);
+		return ResponseEntity.ok().body(entity);
 	}
 }
--- src/main/java/com/dio/productCatalog/services/ProductService.java
+++ src/main/java/com/dio/productCatalog/services/ProductService.java
@@ -1,11 +1,14 @@
 package com.dio.productCatalog.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dio.productCatalog.entities.Product;
 import com.dio.productCatalog.repositories.ProductRepository;
+import com.dio.productCatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ProductService {
@@ -17,5 +20,11 @@ public class ProductService {
 	public Product create(Product product) {
 		Product newProduct = repository.save(product);
 		return newProduct;
+	}
+	
+	@Transactional(readOnly = true)
+	public Product findById(Integer id) {
+		Optional<Product> foundEntity = repository.findById(id);
+		return foundEntity.orElseThrow(() -> new EntityNotFoundException("id " + id + " not found"));
 	}
 }
